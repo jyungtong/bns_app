0cc720edb8922eea6cb4ce06b0db5d7d06193442
tuxtong <huntthetux@gmail.com>
2013-03-06 15:43:04 +0800

    validates presence of profile info

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,6 +3,12 @@ class Profile < ActiveRecord::Base
 									:facebook_name
 	belongs_to :user
 
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :contact_number, :presence => true
+  validates :national_identity, :presence => true
+  validates :facebook_name, :presence => true
+
 	def full_name
 		"#{first_name} #{last_name}"
 	end
